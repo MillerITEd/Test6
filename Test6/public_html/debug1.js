fef2a5ac7951dd6fef8ccf21b4914fb2827cf512
Kelli Miller <49914948+MillerITEd@users.noreply.github.com>
2019-11-11 20:51:10 -0600

    Add files via upload

---
+++ Test6/public_html/debug1.js
@@ -0,0 +1,14 @@
+//This program computes the sum of 2 given integers.
+//If the two values are the same, then it returns triple their sum.
+
+var number1 = prompt("Enter the first number");
+var number2 = prompt("Enter the second number")
+
+var sum = number1 + number2;
+if (number1 = number2) {
+    document.write(3 * sum);
+}
+else 
+    document.write (sum);
+}
+
